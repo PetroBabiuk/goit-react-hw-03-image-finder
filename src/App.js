c19e978d272ef7ff5960102d7dee66d6800fbe52
Petro Babiuk <75635325+PetroBabiuk@users.noreply.github.com>
2021-09-02 08:54:43 +0300

    move fetches into ImageGalary and create state machine

--- src/App.js
+++ src/App.js
@@ -1,160 +1,29 @@
 import React, {Component} from 'react';
-// import shortid from 'shortid';
 import { ToastContainer } from 'react-toastify';
 import './App.css';
 import Searchbar from './components/Searchbar';
-// import SearchForm from './components/SearchForm';
 import ImageGallery from './components/ImageGallery';
-import Loader from "react-loader-spinner";
-import Button from 'components/Button';
-// import ContactList from './components/ContactList';
 
 class App extends Component {
   static propTypes = {};
 
   state = {
     query: '',
-    page: 1,
-    keyid: '22394687-5b263f11c9e1c3bf9700990e1',
-    button: true,
-    status: 'idle',
-    // url: `https://pixabay.com/api/?q=${this.state.query}&page=${this.state.page}&key=${this.state.keyid}&image_type=photo&orientation=horizontal&per_page=12`,
   };
 
   getQuery = query => {
     this.setState({ query });
   }
 
-  handleLoadMore = (e) => {
-    this.setState((prevState) => {
-      return {
-        page: prevState.page + 1,
-      }
-    });
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevState.query;
-    const nextQuery = this.state.query;
-    if (prevQuery !== nextQuery) {
-      this.setState({ status: 'pending' });
-      fetch(`https://pixabay.com/api/?q=${this.state.query}&page=${this.state.page}&key=${this.state.keyid}&image_type=photo&orientation=horizontal&per_page=12`)
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          }
-          return Promise.reject(new Error(`Something was wrong, please reload the page`));
-        })
-        .then(images => {
-          console.log(images);
-          if (images.totalHits === 0) {
-            return Promise.reject(new Error(`Nothing with name ${this.state.query} was not found`));
-          }
-            this.setState({ images: images.hits, status: 'resolved'});
-        })
-        .catch(error => this.setState({ error, status: 'rejected' }))
-    }
-  }
-
   render() {
-    const { images, error, status, button } = this.state;
-
-    if (status === 'idle') {
-      return (
-        <div>
-          <Searchbar onSubmit={this.getQuery} />
-          <h1>Please enter something in input</h1>
-          <ToastContainer
-            position="top-right"
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-          />
-        </div>
-      );
-    }
-
-    if (status === 'pending') {
-      return (
-        <div>
-          <Searchbar onSubmit={this.getQuery} />
-          <Loader
-        type="Puff"
-        color="#00BFFF"
-        height={280}
-        width={280}
-      // timeout={3000} //3 secs
-        />
-        </div>
-        
-      );
-    }
-
-    if (status === 'rejected') {
-      return (
-        <div>
-          <Searchbar onSubmit={this.getQuery} />
-          <h1>{error.message}</h1>
-          <ToastContainer
-            position="top-right"
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-          />
-        </div>);
-    }
-
-    if (status === 'resolved') {
-      return (
-        <div>
-          <Searchbar onSubmit={this.getQuery} />
-          <ImageGallery images={images} />
-          {button && <Button
-            onClick={this.handleLoadMore}
-          />}
-          <ToastContainer
-            position="top-right"
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-          />
-        </div>);
-    }
-
-
     return (
       <div>
-        { button && <Button
-          onClick={this.handleLoadMore}
-        />}
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </div>
-        
+        <Searchbar onSubmit={this.getQuery} />
+        {/* <h1>Please enter something in input</h1> */}
+        <ImageGallery query={this.state.query}/>
+        {/* <h1>{error.message}</h1> */}
+        <ToastContainer />
+      </div>   
     )
   }
 }
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,19 +1,135 @@
-import React from 'react';
+import { Component } from 'react';
 import PropTypes from 'prop-types';
+import Loader from "react-loader-spinner";
+import Button from 'components/Button';
 import ImageGalleryItem from '../ImageGalleryItem';
+import picturesApi from '../../servises/picturesApi';
 
-const ImageGallery = ({ images }) => (
-    <ul className="ImageGallery">
-        {images.map(image => (
-            <ImageGalleryItem
-                key={image.id}
-                id={image.id}
-                // largeImageURL={image.largeImageURL}
-                webformatURL={image.webformatURL}
-            />
-        ))}
-    </ul>
-);
+class ImageGallery extends Component {
+
+    state = {
+        images: [],
+        error: null,
+        pages: 0,
+        page: 1,
+        status: 'idle',
+    };
+
+    async componentDidUpdate(prevProps, prevState) {
+        const prevQuery = prevProps.query;
+        const prevPage = prevState.page;
+        const nextQuery = this.props.query;
+        const nextPage = this.state.page;
+
+        if (prevQuery !== nextQuery) {
+            this.setState({ status: 'pending', page: 1 })
+            await picturesApi(nextQuery, prevPage)
+                .then(images => {
+                    if (images.totalHits === 0) {
+                        throw new Error(`Nothing with name ${nextQuery} was not found`);
+                    }
+                    console.log(images);
+                    this.setState((prevState) => ({
+                        images: [...images.hits],
+                        status: 'resolved',
+                        pages: images.totalHits/12,
+                    }));
+                })
+                .catch(error => this.setState({ error, status: 'rejected' }));
+        }
+
+        if (prevPage !== nextPage && nextPage > 1) {
+            this.setState({ status: 'another-pending'})
+            await picturesApi(nextQuery, nextPage)
+                .then(images => {
+                    console.log(images);
+                    this.setState((prevState) => ({ images: [...prevState.images, ...images.hits], status: 'resolved', }));
+                })
+                .catch(error => this.setState({ error, status: 'rejected' }));
+        }
+    }
+
+    autoScroll = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth',
+    });
+  };
+
+  handleLoadMore = () => {
+      this.setState((prevState) =>{
+          return {page: prevState.page + 1}
+      })
+  };
+
+    render() {
+        const { images, error, status, pages, page} = this.state;
+
+        if (status === 'idle') {
+            return (
+                <h1>Please, enter something</h1>
+            );
+        }
+
+        if (status === 'pending') {
+            return (
+                <Loader
+                    type="ThreeDots"
+                    color="#3f51b5"
+                    height={280}
+                    width={280}
+                />
+            );
+        }
+
+        if (status === 'another-pending') {
+            return (
+                <>
+                    <ul className="ImageGallery">
+                        {images.map(image => (
+                            <ImageGalleryItem
+                                key={image.id}
+                                id={image.id}
+                                // largeImageURL={image.largeImageURL}
+                                webformatURL={image.webformatURL}
+                            />
+                        ))}
+                    </ul>
+                    <Loader
+                        type="ThreeDots"
+                        color="#3f51b5"
+                        height={280}
+                        width={280}
+                    />
+                </>
+            );
+        }
+
+        if (status === 'rejected') {
+            return (
+                <h1>{error.message}</h1>
+            )
+        }
+
+        if (status === 'resolved') {
+            return (
+                <>
+                    <ul className="ImageGallery">
+                        {images.map(image => (
+                            <ImageGalleryItem
+                                key={image.id}
+                                id={image.id}
+                                // largeImageURL={image.largeImageURL}
+                                webformatURL={image.webformatURL}
+                            />
+                        ))}
+                    </ul>
+                    {pages >= page ? <Button onClick={this.handleLoadMore} /> : <></>}
+                </>
+            );
+        }
+    }
+}
 
 ImageGallery.propTypes = {
     images: PropTypes.arrayOf(PropTypes.object),
